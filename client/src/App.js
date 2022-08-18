3bf5991473623a3d32acec8b9d08140bb72a52c8
CalTea <caltees1@gmail.com>
2022-08-18 18:54:00 +0100

    Setting up react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+const App = ()  => {
+  return (
+    <>
+    <Header />
+    <main>
+      <h1>Cals Shop</h1>
+    </main>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
