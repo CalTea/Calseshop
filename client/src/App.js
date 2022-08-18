d7b45987b0f063701f71457aceaaac3cebc5216b
CalTea <caltees1@gmail.com>
2022-08-18 20:56:20 +0100

    Added product and home screen components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {Container} from 'react-bootstrap'
 import './bootstrap.min.css'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from './screens/HomeScreen';
 
 const App = ()  => {
   return (
@@ -9,7 +10,7 @@ const App = ()  => {
     <Header />
     <main className='py-3'>
       <Container>
-        <h1>Cals Shop</h1>
+        <HomeScreen />
       </Container>
     </main>
     <Footer />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand bg='light' href="/">Cal's Shop</Navbar.Brand>
+        <Navbar.Brand href="/">Cal's Shop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto" >
